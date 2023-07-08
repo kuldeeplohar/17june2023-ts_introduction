35663a1c3800cc37a3bba9419a155268d614409a
kuldeeplohar <loharkuldeep169@gmail.com>
2023-07-08 13:39:56 +0530

    ts

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+var name1 = "kuldeep";
+console.log(name1);
+var age = 10;
+console.log(age);
---
+++ js/custom.ts
@@ -0,0 +1,8 @@
+
+
+var name1:string="kuldeep"
+ 
+console.log(name1);
+
+let age:number=10
+console.log(age)
